ca746a367a673f508994f4b14d7db7309cb556c4
WayneZ <waynez@login14.chn.perlmutter.nersc.gov>
2024-07-12 17:16:20 -0700

    adding ssh tunnel

---
+++ .ipynb_checkpoints/FindMyFW-checkpoint.py
@@ -0,0 +1,80 @@
+import os
+import subprocess
+import json
+
+# Function to execute shell commands and return the output
+
+def execute_command(command):
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        if result.returncode != 0:
+            raise Exception(f"Command failed: {command}\n{result.stderr}")
+        return result.stdout.strip()
+    except Exception as e:
+        print(e)
+        ssh_login()
+        return None
+
+def ssh_login():
+    hostname = input("Enter the hostname without your username: ")
+    username = input("Enter the username: ")
+    os.system(f"ssh {username}@{hostname}")
+
+def get_fwid(job_id):
+
+    # Get job information and save to a temporary file
+    job_info = execute_command(f"scontrol show jobid {jobid}")
+
+    # Extract the StdOut directory from the job information
+    stdout_dir = ""
+    for line in job_info.splitlines():
+        if "StdOut=" in line:
+            stdout_dir = line.split("=", 1)[1]
+            break
+
+    # Check if StdOut path was found
+    if not stdout_dir:
+        print("StdOut path not found in job information")
+        exit(1)
+
+    base_dir = os.path.dirname(stdout_dir)
+
+    # Change directory to the extracted base directory
+    try:
+        os.chdir(base_dir)
+    except OSError:
+        print(f"Failed to change directory to {base_dir}")
+        exit(1)
+
+    # Print the current directory to confirm
+    print(f"Changed directory to: {os.getcwd()}")
+
+    # Find the largest directory with the pattern "launcher_*"
+    launch_dirs = subprocess.check_output(f"find {os.getcwd()} -type d -name 'launcher_*'", shell=True).decode().splitlines()
+    largest_dir = max(launch_dirs, key=lambda d: d.split('_')[-1])
+
+    try:
+        os.chdir(largest_dir)
+    except OSError:
+        print(f"Failed to change directory to {largest_dir}")
+        exit(1)
+
+    print(f"Changed directory to: {os.getcwd()}")
+
+    json_file = os.path.join(os.getcwd(), "FW.json")
+
+    # Check if the JSON file exists
+    if os.path.isfile(json_file):
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+        spec_mpid = data.get('spec', {}).get('MPID', 'N/A')
+        fw_id = data.get('fw_id', 'N/A')
+
+        # Output the extracted values
+        print(f"spec.MPID: {spec_mpid}")
+        print(f"fw_id: {fw_id}")
+    else:
+        print(f"FW.json not found in {largest_dir}")
+        
+        return fw_id
+
